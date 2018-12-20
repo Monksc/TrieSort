8f318107f5d3d0e2792ef4bd2d1fc2cccb7111d7
Cameron Monks <ccmonks32@gmail.com>
2018-12-20 13:38:19 -0500

    First commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  TrieSort
+//
+//  Created by Cameron Monks on 12/20/18.
+//  Copyright © 2018 Cameron Monks. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    
+    std::cout << "HELLO WORLD" << std::endl;
+    
+    return 0;
+}
